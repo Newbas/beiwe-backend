733d36cefb93be58ea4de2615b0aa517c7ac352d
Eli Jones <eli@zagaran.com>
2021-09-11 03:44:04 -0400

    [view-study-pageload-fix] pulls database queries out of main loop into lookup dicts (untested because of database issues, sorry)

--- api/study_api_helpers.py
+++ api/study_api_helpers.py
@@ -1,6 +1,10 @@
+from collections import defaultdict
+
 from django.db.models import BooleanField, ExpressionWrapper, Q
 from django.db.models.functions import Lower
-from database.user_models import Participant
+
+from database.schedule_models import InterventionDate
+from database.user_models import Participant, ParticipantFieldValue
 
 
 def get_values_for_view_study_table(
@@ -22,6 +26,20 @@ def get_values_for_view_study_table(
         [start: start + length]
     )
 
+    # InterventionDates and ParticipantFieldValue can be stuck into lookup dictionaries, to avoid
+    # database calls in the inner loop.
+    intervention_query = InterventionDate.objects.filter(participant__in=query)
+    field_value_query = ParticipantFieldValue.objects.filter(participant__in=query)
+    # don't convert these to regular dicts, in the case of no values it instantiates an empty list
+    # inside the main loop, and then skips over it - otherwise we would get a KeyError.
+    intervention_dates = defaultdict(list)
+    field_values = defaultdict(list)
+    # ordering by intervention / field name is sufficient, don't need to order by participant.
+    for intervention in intervention_query.order_by(Lower('name')):
+        intervention_dates[intervention.participant_id].append(intervention)
+    for field_value in field_value_query.order_by(Lower('field_name')):
+        field_values[field_value.participant_id].append(field_value)
+
     participants_data = []
     for participant in query:
         # Get the list of the basic columns, which are present in every study.
@@ -29,13 +47,13 @@ def get_values_for_view_study_table(
         # Convert the datetime object into a string in YYYY-MM-DD format
         participant_values[0] = participant_values[0].strftime('%Y-%m-%d')
         # Add all values for intervention dates
-        for intervention_date in participant.intervention_dates.order_by(Lower('intervention__name')):
+        for intervention_date in intervention_dates[participant.id]:
             if intervention_date.date is not None:
                 participant_values.append(intervention_date.date.strftime('%Y-%m-%d'))
             else:
                 participant_values.append(None)
         # Add all values for custom fields
-        for custom_field_val in participant.field_values.order_by(Lower('field__field_name')):
+        for custom_field_val in field_values[participant.id]:
             participant_values.append(custom_field_val.value)
         participants_data.append(participant_values)
 
